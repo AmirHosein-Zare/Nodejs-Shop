024d8e41ddeadf73961eb5915cdd757a88a2b619
AmirHosein <awir.software@gmail.com>
2023-02-18 17:01:07 +0330

    finish db startup

--- startup/db.js
+++ startup/db.js
@@ -1,1 +1,8 @@
 const config = require('config');
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.set('strictQuery', false);
+    mongoose.connect(config.get('db'))
+    .then(()=>{console.log('connected to mongodb...');})
+}
