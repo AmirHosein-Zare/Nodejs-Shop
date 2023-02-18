4fca5302617c86f443c33a6a5c9cf54639baac08
AmirHosein <awir.software@gmail.com>
2023-02-18 17:02:23 +0330

    set mongoose on inde

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//startup
+require('./startup/db')();
+
 // set port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`);});
