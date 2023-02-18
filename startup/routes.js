429bb5394410d90419b41b5d7b0b48c8f904dfb3
AmirHosein <awir.software@gmail.com>
2023-02-18 18:16:04 +0330

    add morgan

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 module.exports = () => {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-    app.use(express.static('public'));
+    app.use(morgan('tiny'));
 }
