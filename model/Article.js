c753cea913f8d88fba218a123d14d89ad34c3c2d
AmirHosein <awir.software@gmail.com>
2023-02-20 01:02:07 +0330

    add ArticleSchema

---
+++ model/Article.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const {UserSchema} = require('./User');
+
+const ArticleSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    author:{
+        type: UserSchema
+    },
+    description:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    photos:{
+        type: [String]
+    }
+});
