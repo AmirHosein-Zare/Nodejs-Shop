d49ffd3dab396576611e243fe4015744b2d56dd2
AmirHosein <awir.software@gmail.com>
2023-02-20 00:40:35 +0330

    add Conactus Schema

---
+++ model/Contactus.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// regular expression for email
+let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+
+const ContactusSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3
+    },
+    email:{
+        type: String,
+        trim: true,
+        required: true,
+        match: regex
+    },
+    message:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
