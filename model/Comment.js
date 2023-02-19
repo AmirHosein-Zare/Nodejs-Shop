2cd2328732b6384204e0aa32c0f1351155083441
AmirHosein <awir.software@gmail.com>
2023-02-20 00:22:35 +0330

    add comment schema

---
+++ model/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const {UserSchema} = require('./User');
+const {ProductSchema} = require('./Product');
+
+const CommentSchema = new mongoose.Schema({
+    User:{
+        type: UserSchema
+    },
+    Product:{
+        type: ProductSchema
+    },
+    message:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
