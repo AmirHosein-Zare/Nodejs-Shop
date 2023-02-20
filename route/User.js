27cc5dc6819cb2613409b83fdea54b5eed72c00a
AmirHosein <awir.software@gmail.com>
2023-02-20 13:22:27 +0330

    edit get apis

--- route/User.js
+++ route/User.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const {User} = require('../model/User');
 
-router.get('/api/users', (req, res) => {
+router.get('/', (req, res) => {
     const users = User.find();
     if(!users) return res.status(404).send('can\'t find any user');
     
     res.send(users);
 });
 
-router.get('/api/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const user = User.findById(req.params.id);
     if(!user) return res.status(404).send('user Not Found!');
 
     res.send(user);
 });
+
