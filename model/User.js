6b0ceca2757106a054a12817e0d229110adfd073
AmirHosein <awir.software@gmail.com>
2023-02-20 10:26:25 +0330

    edit User model

--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const {CommentSchema} = require('./Comment');
+const {OrderSchema} = require('./Order');
 
 // regular expression for email
 let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
@@ -41,15 +43,14 @@ const UserSchema = new mongoose.Schema({
         minlength: 6,
         maxlength: 255
     },
-    comments: [String], // replace it with Comment Model
+    comments: [CommentSchema], // replace it with Comment Model
     address:{
         type: String,
         trim: true,
         minlength: 10,
         maxlength: 555
     },
-    Order: [String], // replace it with Order Model
-    Comments: [String] // replace it with Comment Model
+    Order: [OrderSchema], // replace it with Order Model
 });
 
 const User = mongoose.model('User', UserSchema);
