736351c7f5527652e6243a132292961f6cf49839
AmirHosein <awir.software@gmail.com>
2023-02-22 19:21:35 +0330

    add put article api

--- route/Article.js
+++ route/Article.js
@@ -35,3 +35,25 @@ router.post('/', async(req, res) => {
     await article.save();
     res.send(article);
 });
+
+// PUT API
+router.put('/:id', async(req, res) => {
+    // check article
+    const article = await Article.findById(req.params.id);
+    if(!article) return res.status(404).send('Article Not Found');
+
+    // check validation
+    const {error} = isValidArticle(req.body);
+    if(error) return res.status(404).send('Article Not Valid');
+
+    // update Article
+    const newArticle = await Article.findByIdAndUpdate(req.params.id, {
+        $set:{
+            title: newArticle.title,
+            description: newArticle.description,
+            author: User.findById(newArticle.userId)
+        }
+    }, {new: true});
+
+    res.send(newArticle);
+})
