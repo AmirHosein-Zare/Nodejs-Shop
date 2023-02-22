1fb0642cdbc0da6d4f8848722d167b9283e50377
AmirHosein <awir.software@gmail.com>
2023-02-22 15:12:27 +0330

    add contact us get all api

--- route/Contactus.js
+++ route/Contactus.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {Contactus, isValidContactus} = require('../model/Contactus');
 
+//get all contactus api
+router.get('/', async(req, res) => {
+    const messages = await Contactus.find();
+    if(!messages) return res.status(404).send('Messages Not Found');
+
+    res.send(messages);
+})
