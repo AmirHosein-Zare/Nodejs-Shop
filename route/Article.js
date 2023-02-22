084dd9e07ab367076b02dc37dadf2636ca48f240
AmirHosein <awir.software@gmail.com>
2023-02-22 14:17:18 +0330

    add get article by id api

--- route/Article.js
+++ route/Article.js
@@ -9,3 +9,11 @@ router.get('/', async(req, res) => {
 
     res.send(articles);
 });
+
+//get article by id api
+router.get('/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id);
+    if(!article) return res.status(404).send('Article Not Found');
+
+    res.send(article);
+});
