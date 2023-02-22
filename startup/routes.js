e6b6490a2cbf934fffffd8fa61375c0027eeef62
AmirHosein <awir.software@gmail.com>
2023-02-22 13:41:24 +0330

    add product routes to startup

--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const UserRoutes = require('../route/User');
+const ProductRoutes = require('../route/Product');
 
 module.exports = () => {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(morgan('tiny'));
     app.use('/api/users', UserRoutes);
+    app.use('/api/products', ProductRoutes);
 }
