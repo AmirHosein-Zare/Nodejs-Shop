a63643ac42bdc2761a99262cc57582f8c2bf3103
AmirHosein <awir.software@gmail.com>
2023-02-28 21:19:23 +0330

    set login api for user

---
+++ route/login.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {User} = require('../model/User');
+
+// login request
+router.post('/', async(req, res) => {
+    // find user with username
+    const user = await User
+        .find({username: req.body.name})
+        .select({username: 1, password: 1});
+    if(!user) return res.status(404).send('User Not Found');
+    // check password
+    if(req.body.password === user.password){
+        res.status(200).header('x-header-auth', await user.getJwt());
+    }
+    else{
+        res.status(400).send('username or password is incorrect');
+    }
+});
