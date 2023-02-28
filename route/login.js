404841348b2449339abe06906c42ae20db4270dd
AmirHosein <awir.software@gmail.com>
2023-02-28 22:05:22 +0330

    some change

--- route/login.js
+++ route/login.js
@@ -6,12 +6,15 @@ const {User} = require('../model/User');
 router.post('/', async(req, res) => {
     // find user with username
     const user = await User
-        .find({username: req.body.name})
+        .findOne({username: req.body.username})
         .select({username: 1, password: 1});
+    console.log(user);
     if(!user) return res.status(404).send('User Not Found');
     // check password
     if(req.body.password === user.password){
-        res.status(200).header('x-header-auth', await user.getJwt());
+        console.log("true");
+        const token = await user.getJwt();
+        res.status(200).header('x-header-auth', token);
     }
     else{
         res.status(400).send('username or password is incorrect');
