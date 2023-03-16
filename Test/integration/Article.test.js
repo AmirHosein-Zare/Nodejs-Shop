3998ff092e681ad45d6f1c4dce79123ed9c4af9c
AmirHosein <awir.software@gmail.com>
2023-03-16 14:51:01 +0330

    initialize article integration test file

--- Test/integration/Article.test.js
+++ Test/integration/Article.test.js
@@ -1,4 +1,11 @@
+const {Article} = require('../../model/Article');
+const request = require('supertest');
+let server;
 
-describe('GET /', () => {
-    
+describe('article integration test', () => {
+    beforeEach(() => {server = require('../../index')});
+    afterEach( async () => {
+        server.close();
+        await Article.remove({});
+    })
 })
