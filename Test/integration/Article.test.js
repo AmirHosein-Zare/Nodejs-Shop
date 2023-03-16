577d6ea618ca00a3a1d8831b0f62f0c8010120f8
AmirHosein <awir.software@gmail.com>
2023-03-16 16:31:30 +0330

    add test for get all data

--- Test/integration/Article.test.js
+++ Test/integration/Article.test.js
@@ -8,4 +8,27 @@ describe('article integration test', () => {
         server.close();
         await Article.remove({});
     })
+
+    describe('GET /', () => {
+        it('send valid data and get all data', async () => {
+            await Article.insertMany([
+                {title: 'numer1', description:'ssssssssssssssssssssssssss'},
+                {title: 'numer2', description:'ssssssssssssssssssssssssss'},
+                {title: 'numer3', description:'ssssssssssssssssssssssssss'}
+            ]);
+
+            const res = await request(server).get('/api/articles');
+
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(3);
+            expect(res.body.some(article => article.title === 'numer1')).toBeTruthy();
+        })
+
+        it('not found any articles and send 404 status code', async () => {
+            const res = await request(server).get('/api/articles');
+
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(0);
+        })
+    })
 })
