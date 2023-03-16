21d7b39f70f3a66b029ae0bd502b382f6ea474d0
AmirHosein <awir.software@gmail.com>
2023-03-16 16:42:48 +0330

    edit on objectId middleware

--- middleware/objectId.js
+++ middleware/objectId.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = (req, res, next) => {
     if(!mongoose.Types.ObjectId.isValid(req.params.id)){
-        return res.status(401).send('invalid id');
+        return res.status(404).send('invalid id');
     }
 
     next();
