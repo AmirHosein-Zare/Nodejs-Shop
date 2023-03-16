0d0c2983d2b80cb0decc4d3e91774229d2cc2755
AmirHosein <awir.software@gmail.com>
2023-03-16 16:45:35 +0330

    add get by id test on articles

--- Test/integration/Article.test.js
+++ Test/integration/Article.test.js
@@ -9,7 +9,7 @@ describe('article integration test', () => {
         await Article.remove({});
     })
 
-    describe('GET /', () => {
+    describe('GET / ------------------------------', () => {
         it('send valid data and get all data', async () => {
             await Article.insertMany([
                 {title: 'numer1', description:'ssssssssssssssssssssssssss'},
@@ -29,6 +29,28 @@ describe('article integration test', () => {
 
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(0);
+        })
+    })
+
+    describe('GET /:id----------------------------', () => {
+        it('send a valid id', async () => {
+            const article = new Article({
+                title: 'number1', 
+                description: 'fdfsdfdsfdsfdsfsdf'
+            })
+    
+            await article.save();
+    
+            const res = await request(server).get('/api/articles/' + article._id);
+    
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('title', 'number1');
+        })
+
+        it('send invalid id and get 404', async () => {
+            const res = await request(server).get('/api/articles/1');
+
+            expect(res.status).toBe(404);
         })
     })
 })
